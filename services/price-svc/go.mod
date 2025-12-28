37649fb27f409e7411fbae62628cb8ba0e88ec76
Evgeniy <121779561+NightRunnerEB@users.noreply.github.com>
2025-12-28 23:50:18 +0300

    price-svc: scaffold module with proto contracts and migrations

--- services/price-svc/go.mod
+++ services/price-svc/go.mod
@@ -1,3 +1,32 @@
 module github.com/NightRunner/CryptoTax-Go/services/price-svc
 
 go 1.25.1
+
+require (
+	google.golang.org/grpc v1.78.0
+	google.golang.org/protobuf v1.36.11
+)
+
+require (
+	github.com/Masterminds/squirrel v1.5.4 // indirect
+	github.com/caarlos0/env/v11 v11.3.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.8.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/redis/go-redis/v9 v9.17.2 // indirect
+	github.com/rs/zerolog v1.34.0 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sync v0.18.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251029180050-ab9386a59fda // indirect
+)
---
+++ services/price-svc/internal/config/config.go
@@ -0,0 +1,66 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/caarlos0/env/v11"
+	"github.com/joho/godotenv"
+)
+
+type (
+	Config struct {
+	}
+
+	App struct {
+		Name    string `env:"APP_NAME" envDefault:"price-svc"`
+		Version string `env:"APP_VERSION,required"`
+		Env     string `env:"APP_ENV" envDefault:"dev"`
+	}
+
+	PG struct {
+		URL            string        `env:"DATABASE_URL,required"`
+		PoolMax        int           `env:"DB_MAX_CONNS" envDefault:"10"`
+		ConnectTimeout time.Duration `env:"DB_CONN_TIMEOUT" envDefault:"3s"`
+		AttemptTimeout time.Duration `env:"DB_ATTEMPT_TIMEOUT" envDefault:"1s"`
+	}
+
+	RedisConfig struct {
+		RedisURL string        `env:"REDIS_URL,required"`
+		PoolMax  int           `env:"REDIS_POOL_MAX" envDefault:"4"`
+		Skew     time.Duration `env:"REDIS_SKEW_SECS" envDefault:"5s"`
+	}
+
+	GRPC struct {
+		Addr string `env:"GRPC_ADDR" envDefault:":8091"`
+	}
+
+	Log struct {
+		Level string `env:"LOG_LEVEL" envDefault:"info"`
+	}
+
+	// Metrics struct {
+	// 	Enabled bool `env:"METRICS_ENABLED" envDefault:"true"`
+	// }
+
+	// Swagger struct {
+	// 	Enabled bool `env:"SWAGGER_ENABLED" envDefault:"false"`
+	// }
+)
+
+func NewConfig() (*Config, error) {
+	if os.Getenv("APP_ENV") != "prod" {
+		if err := godotenv.Load(); err != nil {
+			return nil, fmt.Errorf("failed to load .env file: %w", err)
+		}
+	}
+
+	cfg := &Config{}
+
+	if err := env.Parse(cfg); err != nil {
+		return nil, fmt.Errorf("failed to parse config: %w", err)
+	}
+
+	return cfg, nil
+}
