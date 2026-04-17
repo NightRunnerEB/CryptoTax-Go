bb64fc10f0a7aced2171ef41d5206a6212a7eabc
Evgeniy <121779561+NightRunnerEB@users.noreply.github.com>
2026-04-17 16:30:00 +0600

    feat(frontend): adopt generated application shell

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,19 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './app/AppRouter'
 import { AuthProvider } from './auth/AuthContext'
 import { NotificationProvider } from './components/notifications/NotificationProvider'
+import { ThemeProvider } from './components/theme/ThemeProvider'
 
 function App() {
   return (
-    <NotificationProvider>
-      <AuthProvider>
-        <BrowserRouter>
-          <AppRouter />
-        </BrowserRouter>
-      </AuthProvider>
-    </NotificationProvider>
+    <ThemeProvider>
+      <NotificationProvider>
+        <AuthProvider>
+          <BrowserRouter>
+            <AppRouter />
+          </BrowserRouter>
+        </AuthProvider>
+      </NotificationProvider>
+    </ThemeProvider>
   )
 }
 
